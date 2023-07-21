0cbb56a128791cd1f6cd02662cac770ceb377773
Julius Gyeti 10989434 <102974538+GyetiJulius@users.noreply.github.com>
2023-07-21 10:41:58 -0900

    Add files via upload

---
+++ views/script.js
@@ -0,0 +1,25 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Toggle active class on navigation links based on scroll position
+window.addEventListener('scroll', function () {
+  const navLinks = document.querySelectorAll('nav a');
+  const currentSection = window.scrollY + 300;
+
+  navLinks.forEach(link => {
+    const section = document.querySelector(link.getAttribute('href'));
+    if (section.offsetTop <= currentSection && section.offsetTop + section.offsetHeight > currentSection) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+});
